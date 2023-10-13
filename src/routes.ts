a09a1fae290d6b47e43e33b602916f2877939dbf
tomaszsliwakowski <tomek08082002@gmail.com>
2023-10-13 19:58:59 +0200

    Changes to be committed: 	modified:   src/routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,11 @@
+export const SERVER = "https://dashboard-e-shop.vercel.app";
+export const LOCAL_SERVER = "http://localhost:8080";
+
 export const PANEL_ROUTE = "/dashboard";
 export const SALE_ROUTE = "/dashboard/sale";
 export const PRODUCTS_ROUTE = "/dashboard/products";
-export const GET_SALE = "http://localhost:8080/api/sale";
+export const GET_SALE = `${SERVER}/api/sale`;
 
 export function DELETE_SALE(id: string) {
-  return `http://localhost:8080/api/sale/${id}`;
+  return `${SERVER}/api/sale/${id}`;
 }
